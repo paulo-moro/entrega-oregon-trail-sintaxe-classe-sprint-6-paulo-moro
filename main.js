b5fd12bbac96c8b064277004c2298a9e1933c95f
Paulo Moro <paulo.morolol@gmail.com>
2022-02-16 08:40:42 -0300

    update: atualizado dados classes criadas no class.js enquanto que as variaveis criadas estão no main.js, adicionado frase na tela inicial para indicar que deve olhar resulado no console.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-2
+let wagon = new Wagon(2);
+// Criar três viajantes
+let henrietta = new Traveler('Henrietta');
+let juan = new Traveler('Juan');
+let maude = new Traveler('Maude');
+
+console.log(`${wagon.getAvailableSeatCount()} should be 2`);
+
+wagon.join(henrietta);
+console.log(`${wagon.getAvailableSeatCount()} should be 1`);
+
+wagon.join(juan);
+wagon.join(maude); // Não tem espaço para ela!
+console.log(`${wagon.getAvailableSeatCount()} should be 0`);
+
+henrietta.hunt(); // pega mais comida
+juan.eat();
+juan.eat(); // juan agora está com fome (doente)
+
+console.log(`${wagon.shouldQuarantine()} should be true since juan is sick`);
+console.log(`${wagon.totalFood()} should be 3`);
